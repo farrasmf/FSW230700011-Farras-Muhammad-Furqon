f2c126765ee37317530cfe36a6534f2b758f3942
farrasmf <farrasmuhammadfurqon@gmail.com>
2023-09-17 17:16:11 +0700

    new

---
+++ routers/home.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const HomeController = require("../controllers/home.controller");
+const home = express.Router();
+
+const homeController = new HomeController;
+home.get("/", homeController.index);
+
+module.exports = home;
